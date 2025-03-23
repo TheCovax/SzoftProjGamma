65ad03b88eb6f23226daf67ed17fa2560035bbc3
Tóth Bence <tothben14@gmail.com>
2025-03-23 22:23:53 +0100

    GombaTest implementalasa

--- Fungorium/src/model/GombaTest.java
+++ Fungorium/src/model/GombaTest.java
@@ -4,7 +4,42 @@ import Fungorium.src.model.tekton.Tekton;
 
 public class GombaTest {
 
+    List<GombaFonal> fonalak;
     Tekton tekton;
+    String owner;
+    int level;
+
+
+    GombaTest() {
+        fonalak = new ArrayList<>(Tekton tekton, String owner);
+        this.tekton = tekton;
+        this.owner = owner;
+        level = 0;
+    }
+
+    void produceSpora() {
+            //TODO
+    }
+
+    void clear() {
+            //TODO
+    }
+
+    void upgradeTest() {
+        if(getLevel() < 4) {
+            setLevel(getLevel() + 1);
+            tekton.removeSpora();
+        }
+        else {
+            System.out.println("A gombatest elérte a maximális fejlettségi szintet, nem lehet tovább fejleszteni!");
+            return;
+        }
+    }
+
+    void setRecursiveConnectivity() {
+            //TODO
+    }
+
 
     //Eltavolit egy sporat a jelenlegi tektonrol es athelyezi a celtektonra
     public void shootSpora(Tekton dst){
@@ -14,4 +49,14 @@ public class GombaTest {
     public void setTekton(Tekton t){
         tekton = t;
     }
+
+    int getLevel() {
+        return this.level;
+    }
+
+    void setLevel(int level) {
+        this.level = level;
+    }
+
+    
 }
