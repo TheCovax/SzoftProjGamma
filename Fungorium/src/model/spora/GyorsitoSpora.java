24e8587f2327b7042c9458a63ffc77b49988b298
Lin WenTao <63066184+saorilin@users.noreply.github.com>
2025-03-23 16:13:59 +0100

    Implemented Rovar class and its testcases
    1. Implemented Rovar class, Spora class and its derived classes.
    2. Added some basic function, list to Tekton class.
    3. Implemented clean(), getOtherEnd() for Gombafonal
    4. Implemented Rovar related testcases

--- Fungorium/src/model/spora/GyorsitoSpora.java
+++ Fungorium/src/model/spora/GyorsitoSpora.java
@@ -1,4 +1,18 @@
 package Fungorium.src.model.spora;
 
-public class GyorsitoSpora {
+import Fungorium.src.model.Rovar;
+import Fungorium.src.utility.Logger;
+
+public class GyorsitoSpora extends Spora {
+    public static final int DEFAULT_SPEED_UP_VALUE = 3;
+
+    @Override
+    public  void applyEffect(Rovar r){
+        Logger.methodCall("GyorsitoSpora.applyEffect()");
+
+        r.setSpeed(DEFAULT_SPEED_UP_VALUE);
+        r.setDuration(DEFAULT_EFFECT_DURATION);
+
+        Logger.methodReturn("Gyorsito.applyEffect()");
+    };
 }
--- Fungorium/src/model/tekton/Tekton.java
+++ Fungorium/src/model/tekton/Tekton.java
@@ -1,22 +1,25 @@
 package Fungorium.src.model.tekton;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 import Fungorium.src.model.GombaFonal;
+import Fungorium.src.model.Rovar;
 import Fungorium.src.model.spora.Spora;
+import Fungorium.src.utility.Logger;
 
 public class Tekton {
 
 	Queue<Spora> sporak;
 	List<GombaFonal> fonalak;
+	List<Tekton> neighbours;
+	List<Rovar> rovarok;
 
 	//Konstruktor igeny szerint ki kell egesziteni
 	public Tekton(){
 		sporak = new LinkedList<>();
 		fonalak = new ArrayList<>();
+		neighbours = new ArrayList<>();
+		rovarok = new ArrayList<>();
 	}
 
 	public List<GombaFonal> getFonalak(){
@@ -48,8 +51,74 @@ public class Tekton {
 		return sporak.poll();
 	}
 
+	public Spora peekSpora(){return sporak.peek();}
+
 	public void addGombaFonal(GombaFonal f){
 		fonalak.add(f);
 	}
 
+	public void removeGombaFonal(GombaFonal f){
+		fonalak.remove(f);
+	}
+
+	public boolean hasGombafonal(GombaFonal gf){ return fonalak.contains(gf);}
+
+	public void addNeighbour(Tekton t){
+		neighbours.add(t);
+	}
+
+	public void removeNeighbour(Tekton t){
+		neighbours.remove(t);
+	}
+
+	public void addRovar(Rovar r){
+		rovarok.add(r);
+	}
+
+	public void removeRovar(Rovar r) {
+		rovarok.remove(r);
+	}
+
+	/**
+	 * Visszadja az összes elérhető Tekton-t a jelenlegi tektonból kiindulva (elérhető: össze vannak kapcsolódva gombafonalakkal),
+	 * adott sebesség (lépés-távolság) korlátozásával.
+	 * <p>Az algoritmus BFS (szélességi keresés) segítségével bejárja a tektonokat,
+	 * amelyek gombafonallal kapcsolódnak az aktuális tektonhoz, majd rekurzívan
+	 * folytatja a keresést a következő szinteken, amíg a sebességhatárt el nem éri.</p>
+	 *
+	 * @param speed A keresés mélysége, azaz a maximálisan bejárható távolság.
+	 * @return Egy lista a jelenlegi tektonból elérhető tektonokról.
+	 */
+	public List<Tekton> findReachableTektonWithinDistance(int speed) {
+		Logger.methodCall("tekton.findReachableTektonWithinDistance(speed)");
+
+		List<Tekton> reachable = new ArrayList<>();
+		Queue<Tekton> queue = new LinkedList<>();
+		Set<Tekton> visited = new HashSet<>();
+
+		queue.add(this);
+		visited.add(this);
+
+		int depth = 0;
+
+		while (!queue.isEmpty() && depth < speed){
+			int levelSize = queue.size();
+			for (int i = 0; i < levelSize; i++) {
+				Tekton current = queue.poll();
+
+				for (GombaFonal gf : current.getFonalak()){
+					Tekton connectedTekton = gf.getOtherEnd(current);
+					if(!visited.contains(connectedTekton)){
+						reachable.add(connectedTekton);
+						queue.add(connectedTekton);
+						visited.add(connectedTekton);
+					}
+				}
+			}
+			depth++;
+		}
+
+		Logger.methodReturn("tekton.findReachableTektonWithinDistance(speed)");
+		return reachable;
+	}
 }
