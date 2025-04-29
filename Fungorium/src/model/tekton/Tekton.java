0c3891fb31475d1d0243e587f8be8695a869af1a
Lin WenTao <63066184+saorilin@users.noreply.github.com>
2025-04-29 22:26:03 +0200

    commit

--- Fungorium/src/model/tekton/Tekton.java
+++ Fungorium/src/model/tekton/Tekton.java
@@ -75,7 +75,7 @@ public class Tekton {
 			rovarok = new ArrayList<>();
 			splitRate = splitR;
 
-			id = "T" + idCounter.incrementAndGet();
+			id = generateAutoId();
 		}
 
 	public GombaTest getGombatest() {
